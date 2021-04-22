c9ad5dd663fc721fac11fe6d9428747a393b7cf7
AstroOrbis <65419194+AstroOrbis@users.noreply.github.com>
2021-04-22 08:27:20 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
-import socket, os
+import socket, os, time
   
 hostname = socket.gethostname()   
 IPAddr = socket.gethostbyname(hostname)   
 print("Your Computer Name is:" + hostname)   
 print("Your Computer IP Address is:" + IPAddr)   
 
+time.sleep(2)
 
 os.system("python3 eggshell.py")
